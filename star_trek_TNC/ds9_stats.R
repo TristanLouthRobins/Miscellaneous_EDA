53b261796fe66908309c480b97f424f68ad11cc8
Tristan Louth-Robins <62044678+TristanLouthRobins@users.noreply.github.com>
2023-02-24 10:52:59 +1030

    Update to script
    Current to review of S01 E15 24/2/2023

--- star_trek_TNC/ds9_stats.R
+++ star_trek_TNC/ds9_stats.R
@@ -15,7 +15,7 @@
 # load Tidyverse for data tidying and import current dataset ------------
 library(tidyverse)
 
-data <- read_csv("data/tnc_ds9_stats-230215.csv") %>% 
+data <- read_csv("data/tnc_ds9_stats.csv") %>% 
   slice(1:19)
 
 # tidy data -------------------------------------------------------------
@@ -39,6 +39,52 @@ data %>%
          Matt_MVC = factor(Matt_MVC),
          Andy_Watch = factor(Andy_Watch),
          Matt_Watch = factor(Matt_Watch))
+
+# Add new variables for character's Division (Command, Ops, Sciences, Civilian, Other) 
+
+var.cmd <- c("Sisko", "Worf", "Kira")
+var.ops <- c("O'Brien", "Odo", "Rom")
+var.sci <- c("Dax", "Bashir")
+var.civ <- c("Jake", "Nog", "Keiko", "Quark", "Garak", "Leeta", "Kassidy")
+var.oth <- c("Dukat", "Other")
+
+# Add new variables for character's race (Human, Bajoran, Cardassian, Klingon, Ferengi, Other)
+
+var.hum <- c("Sisko", "O'Brien", "Bashir", "Jake", "Keiko", "Kassidy")
+var.baj <- c("Kira", "Leeta")
+var.car <- c("Garak", "Dukat")
+var.kli <- c("Worf")
+var.fer <- c("Quark", "Rom", "Nog")
+var.tri <- c("Dax")
+var.oth <- c("Changling")
+
+data <- 
+  data %>% 
+  mutate(A.Div = ifelse(Andy_MVC %in% var.cmd, "Command",
+                        ifelse(Andy_MVC %in% var.ops, "Ops",
+                               ifelse(Andy_MVC %in% var.sci, "Sciences",
+                                      ifelse(Andy_MVC %in% var.civ, "Civilian",
+                                             "Other"))))) %>% 
+  mutate(M.Div = ifelse(Matt_MVC %in% var.cmd, "Command",
+                        ifelse(Matt_MVC %in% var.ops, "Ops",
+                               ifelse(Matt_MVC %in% var.sci, "Sciences",
+                                      ifelse(Matt_MVC %in% var.civ, "Civilian",
+                                             "Other"))))) %>% 
+  mutate(A.race = ifelse(Andy_MVC %in% var.hum, "Human",
+                         ifelse(Andy_MVC %in% var.baj, "Bajoran",
+                                ifelse(Andy_MVC %in% var.car, "Cardassian",
+                                       ifelse(Andy_MVC %in% var.kli, "Klingon",
+                                              ifelse(Andy_MVC %in% var.fer, "Ferengi",
+                                                     ifelse(Andy_MVC %in% var.tri, "Trill",
+                                                            "Other"))))))) %>% 
+  mutate(M.race = ifelse(Matt_MVC %in% var.hum, "Human",
+                         ifelse(Matt_MVC %in% var.baj, "Bajoran",
+                                ifelse(Matt_MVC %in% var.car, "Cardassian",
+                                       ifelse(Matt_MVC %in% var.kli, "Klingon",
+                                              ifelse(Matt_MVC %in% var.fer, "Ferengi",
+                                                     ifelse(Matt_MVC %in% var.tri, "Trill",
+                                                            "Other")))))))
+
 
 # summarise the hosts rating data -------------------
 
@@ -65,11 +111,11 @@ pivot_MVC <-
   rename("Character" = "value") %>% 
   select(Host, Character)
 
-MVC_leaderboard <- 
+(MVC_leaderboard <- 
 pivot_MVC %>% 
   group_by(Character) %>% 
   summarise(count = n()) %>% 
-  arrange(desc(count))
+  arrange(desc(count)))
 
 # generate summary statistics ---------------------------------
 (summary_stats <- 
@@ -84,10 +130,62 @@ pivot_MVC %>%
             median = median(Value)) %>% 
   arrange(desc(mean)))
 
+# top 3, bottom 3 eps within given season
+top3 <- function(season, who){
+  data %>% 
+    slice(0:eps_watched) %>% 
+    filter(Season == season) %>% 
+    arrange(desc({{who}})) %>% 
+    head(n=3)
+}
+
+bot3 <- function(season, who){
+  data %>% 
+    slice(0:eps_watched) %>% 
+    filter(Season == season) %>% 
+    arrange({{who}}) %>% 
+    head(n=3)
+}
+  
+(top_TNC <- top3(1,TNC))
+(top_IMDB <- top3(1,IMDB))
+
+(bot_TNC <- bot3(1,TNC))
+(bot_IMDB <- bot3(1,IMDB))
+
+# Summarise MVC by joint count of character division -------------------
+
+pivot_MVC_by_div <- 
+  data %>% 
+  slice(0:eps_watched) %>% 
+  pivot_longer(cols = c("A.Div", "M.Div"), names_to = "Host") %>% 
+  rename("Division" = "value") %>% 
+  select(Host, Division)
+
+(MVC_leaderboardby_div <- 
+    pivot_MVC_by_div %>% 
+    group_by(Division) %>% 
+    summarise(count = n()) %>% 
+    arrange(desc(count))) 
+
+# Summarise MVC by joint count of character race -------------------
+
+pivot_MVC_by_race <- 
+  data %>% 
+  slice(0:eps_watched) %>% 
+  pivot_longer(cols = c("A.race", "M.race"), names_to = "Host") %>% 
+  rename("Race" = "value") %>% 
+  select(Host, Race)
+
+(MVC_leaderboard_by_race <- 
+    pivot_MVC_by_race %>% 
+    group_by(Race) %>% 
+    summarise(count = n()) %>% 
+    arrange(desc(count)))
 
 # visualisations ------------------------------------------------  
 
-library(ggplot)
+library(ggplot2)
 library(cropcircles) # Credit to: https://github.com/doehm for his excellent pkg!
 library(ggimage)
 library(lcars)
@@ -184,12 +282,14 @@ theme_trek_header <- function(){
 # viz for bar plot of the DS9 season 1 MVC leaderboard ------------------
 
 sub_title <- glue("SEASON ONE: MVC VOTE LEADERBOARD ({eps_watched}/19 EPISODES)")
+current_ep <- data$Episode_name[eps_watched]
 
 MVC_plt <- 
 MVC_leaderboard %>% 
   ggplot(aes(x=count, y=fct_reorder(Character, count))) +
   geom_bar(stat = "identity", fill = "#9C9CFF") +
   geom_image(aes(x=count, y=fct_reorder(Character, count), image = img), size = 0.1) +
+  geom_text(aes(x=6.5, y=5, label = toupper(glue("+ 2 VOTES ({current_ep})")), family = "Antonio"), color = "#ED884C", size = 5) +
   labs(
        subtitle = sub_title,
        x = element_blank(),
@@ -309,3 +409,6 @@ patch_plt <-
 patch_plt + plot_annotation(title = 'DEEP SPACE NINE', 
                             subtitle = "STAR TREK: THE NEXT CONVERSATION",
                             caption = "BROUGHT TO YOU BY ODO'S SMOOTH BROW. GITHUB: https://github.com/TristanLouthRobins",theme=theme_trek_header())  
+
+
+
